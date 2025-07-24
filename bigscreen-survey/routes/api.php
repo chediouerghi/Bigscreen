a267ce953926dbfe48ce47952a654c9de317046e
chediouerghi <chediouerghi88@gmail.com>
2025-07-24 11:00:59 +0100

    feat: Mise à jour de la collection Postman et des routes API pour l'authentification et la gestion des sondages

--- bigscreen-survey/routes/api.php
+++ bigscreen-survey/routes/api.php
@@ -1,4 +1,4 @@
-&lt;?php
+<?php
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,19 +9,36 @@ use App\Http\Controllers\SurveyController as PublicSurveyController;
 use App\Http\Controllers\ResponseController;
 use App\Http\Controllers\Admin\DashboardController;
 
+// Auth Routes
 Route::group([
-    'middleware' => 'api',
     'prefix' => 'auth'
 ], function ($router) {
     Route::post('login', [AuthController::class, 'login']);
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::post('refresh', [AuthController::class, 'refresh']);
-    Route::post('me', [AuthController::class, 'me']);
+    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api');
+    Route::post('refresh', [AuthController::class, 'refresh'])->middleware('auth:api');
+    Route::post('me', [AuthController::class, 'me'])->middleware('auth:api');
 });
 
-Route::apiResource('admin/surveys', AdminSurveyController::class)->middleware('auth:api');
-Route::apiResource('admin/surveys.questions', QuestionController::class)->middleware('auth:api');
-Route::get('admin/dashboard', [DashboardController::class, 'index'])->middleware('auth:api');
+// Admin Routes
+Route::group([
+    'prefix' => 'admin',
+    'middleware' => 'auth:api'
+], function () {
+    // Surveys
+    Route::get('surveys', [AdminSurveyController::class, 'index']);
+    Route::post('surveys', [AdminSurveyController::class, 'store']);
+    Route::get('surveys/{survey}', [AdminSurveyController::class, 'show']);
+    Route::put('surveys/{survey}', [AdminSurveyController::class, 'update']);
+    Route::delete('surveys/{survey}', [AdminSurveyController::class, 'destroy']);
 
+    // Questions
+    Route::get('surveys/{survey}/questions', [QuestionController::class, 'index']);
+    Route::post('surveys/{survey}/questions', [QuestionController::class, 'store']);
+
+    // Dashboard
+    Route::get('dashboard', [DashboardController::class, 'index']);
+});
+
+// Public Routes
 Route::get('surveys', [PublicSurveyController::class, 'index']);
 Route::post('surveys/{survey}/responses', [ResponseController::class, 'store']);
